dd5d2ddfd8396ac72790e9533eb29b83fde8fa90
SpoinkyNL <mail@rbeekman.nl>
2020-08-20 00:42:52 +0200

    Modules - Started on custom wizard page

---
+++ src/Module/ModuleProjectWizard/WizardImplementation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace ModuleProjectWizard
+{
+    public class WizardImplementation : IWizard
+    {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            var inputForm = new UserInputForm();
+            inputForm.ShowDialog();
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+    }
+}

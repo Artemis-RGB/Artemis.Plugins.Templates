0d2e8401153f4e74b2921949769b5aacb53560ec
Diogo Trindade <diogotr7@gmail.com>
2024-03-08 15:37:46 +0000

    updated to net 8

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 # This script is used for testing the template locally.
 # It will uninstall the template, clean the template engine user directory, and reinstall the template.
 
-dotnet new --uninstall $PSScriptRoot/src/templates
+dotnet new uninstall $PSScriptRoot/src/templates
 $templateEngineUserDir = (Join-Path -Path $env:USERPROFILE -ChildPath .templateengine)
 Get-ChildItem -path $templateEngineUserDir -directory | Select-Object -ExpandProperty FullName | Remove-Item -recurse -force
-dotnet new --install $PSScriptRoot/src/templates
+dotnet new install $PSScriptRoot/src/templates
